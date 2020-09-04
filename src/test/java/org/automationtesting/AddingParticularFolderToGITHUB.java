b5596b8116aa48de2151554fd025e4770f6b5d26
vijayvj0104 <vijayvj0104@gmail.com>
2020-09-04 17:55:01 +0530

    Adding a Particular Folder Task Completed

---
+++ src/test/java/org/automationtesting/AddingParticularFolderToGITHUB.java
@@ -0,0 +1,10 @@
+package org.automationtesting;
+
+public class AddingParticularFolderToGITHUB {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Adding Particular Folder To GITHUB");
+	}
+
+}
